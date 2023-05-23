0e6bbe05cc2d08c8869b280a036e858f7dd0de41
MD. Ariful Islam <41154962+arif-btkrm@users.noreply.github.com>
2023-05-24 01:28:44 +0600

    Problem 4 AC

--- 52 Prog Problem Book/4. Vajok.cpp
+++ 52 Prog Problem Book/4. Vajok.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+    int n,num;
+    cin >> n;
+    for(int i=1; i<=n;i++){
+        cin >> num;
+        cout << "Case " <<i << ":";
+        for(int j=1; j<=num; j++){
+            if(num%j == 0){
+                cout << " "<<j;
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
